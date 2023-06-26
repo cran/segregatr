933cf21f548f0904826223a919a992c39ed7d7b2
Magnus Dehli Vigeland <m.d.vigeland@medisin.uio.no>
2023-06-26 19:10:02 +0000

    version 0.3.0

--- R/FLB.R
+++ R/FLB.R
@@ -1,11 +1,15 @@
 #' Full-likelihood Bayes factor
 #'
-#' Computes the Bayes factor for co-segregation, as described by Thompson et al.
-#' (2003).
+#' Computes the Bayes factor for co-segregation, as originally described by
+#' Thompson et al. (2003).
 #'
 #' @param x A [pedtools::ped()] object.
 #' @param carriers A character vector (or coercible to such), containing the ID
-#'   labels of pedigree members known to carry the variant in question.
+#'   labels of pedigree members known to carry one copy of the variant in
+#'   question.
+#' @param homozygous A character vector (or coercible to such), containing the
+#'   ID labels of pedigree members known to carry two copies of the variant in
+#'   question.
 #' @param noncarriers A character vector (or coercible to such), containing the
 #'   ID labels of pedigree members known *not* to carry the variant in question.
 #' @param freq A single number strictly between 0 and 1: the population
@@ -14,14 +18,23 @@
 #' @param unknown Pedigree members with unknown affection status.
 #' @param proband The ID label of the proband. This person must also be in both
 #'   `carriers` and `affected`.
-#' @param penetrances Either a numeric vector of length 3, corresponding to
-#'   `(f0, f1, f2)` or a matrix or data frame with 3 columns. Each row contains
-#'   the penetrance values of a liability class.
+#' @param penetrances For autosomal models, a numeric vector of length 3 `(f0,
+#'   f1, f2)`, or a matrix-like with 3 columns, where row `i` contains the
+#'   penetrances of liability class `i`. For X-linked models, a list of two
+#'   vectors named "male" and "female", of lengths 2 `(f0, f1)` and 3 `(f0, f1,
+#'   f2)` respectively. Alternatively, each list entry may be matrix-like (with
+#'   the same number of columns) where each row represents a liability
+#'   class.
 #' @param liability A vector of length `pedsize(x)`, containing for each
 #'   pedigree member the row number of `penetrances` which should be used for
-#'   that individual. (If `penetrances` is just a vector, it will be used for
-#'   all classes.) If `liability` is NULL (the default), it is set to `1` for
-#'   all individuals.
+#'   that individual. (If `penetrances` is just a vector (or one for each sex in
+#'   X-linked models), it will be used for all classes.) If `liability` is NULL
+#'   (the default), it is set to `1` for all individuals.
+#' @param loopBreakers (Relevant only if `x` has loops.) A vector of ID labels
+#'   indicating loop breakers. The default value (NULL) initiates automatic loop
+#'   breaking, which is recommended in most cases.
+#' @param Xchrom A logical, indicating if a model of X-linked inheritance should
+#'   be applied.
 #' @param details A logical, indicating if detailed output should be returned
 #'   (for debugging purposes).
 #' @param plot A logical.
@@ -30,93 +43,156 @@
 #' @references Thompson D, Easton DF, Goldgar DE. *A full-likelihood method for
 #'   the evaluation of causality of sequence variants from family data.* Am J
 #'   Hum Genet, 2003. \doi{10.1086/378100}.
-#' @return A positive number. If `details = TRUE`, a list of intermediate
-#'   results is returned.
+#'
+#' @return A positive number, the FLB score. If `details = TRUE`, a list
+#'   including intermediate results.
 #'
 #' @examples
+#'
+#' ### Autosomal dominant
 #'
 #' x = nuclearPed(2)
 #'
 #' FLB(x, carriers = 3:4, aff = 3:4, unknown = 1:2,
 #'     freq = 0.0001, penetrances = c(0, 1, 1), proband = 3)
 #'
+#'
+#' ### Autosomal recessive with phenocopies and reduced penetrance
+#'
+#' y = nuclearPed(4)
+#'
+#' FLB(y, carriers = 4:5, homozygous = 3, noncarriers = 6,
+#'     aff = 3, unknown = 1:2, freq = 0.0001, proband = 3,
+#'     penetrances = c(0.01, 0.01, 0.99), plot = TRUE)
+#'
+#'
+#' ### X-linked recessive
+#'
+#' z = nuclearPed(3, sex = c(1, 1, 2)) |>
+#'   addChildren(mother = 5, nch = 2, sex = 1:2)
+#'
+#' FLB(z, carriers = c(3, 7), nonc = 4, aff = c(3, 7), unknown = 1:2,
+#'     freq = 0.0001, penetrances = list(male = c(0, 1), female = c(0, 0, 1)),
+#'     proband = 7, Xchrom = TRUE, plot = TRUE)
+#'
 #' @export
-FLB = function(x, carriers, noncarriers = NULL, freq,
-               affected, unknown = NULL, proband,
-               penetrances, liability = NULL,
+FLB = function(x, carriers = NULL, homozygous = NULL, noncarriers = NULL, freq = NULL,
+               affected = NULL, unknown = NULL, proband = NULL,
+               penetrances = NULL, liability = NULL, loopBreakers = NULL, Xchrom = FALSE,
                details = FALSE, plot = FALSE, ...) {
 
-  ### Note to self: Don't mess with the order of input checks.
+  inputs = checkInput(x, affected = affected, unknown = unknown, proband = proband,
+                      carriers = carriers, homozygous = homozygous, noncarriers = noncarriers,
+                      freq = freq, Xchrom = Xchrom, requireProband = TRUE, requireFreq = TRUE)
+  proband = inputs$proband
+  affected = inputs$affected
+  unknown = inputs$unknown
+  carriers = inputs$carriers
+  homozygous = inputs$homozygous
+  noncarriers = inputs$noncarriers
 
-  if(!is.ped(x))
-    stop2("The first argument must be a `ped` object")
-
-  labs = labels(x)
-
-  if(length(proband) == 0 || proband == "")
-    stop2("A proband must be specified")
-  if(!proband %in% labs)
-    stop2("Unknown proband: ", proband)
   if(!proband %in% affected)
     stop2("The proband must be affected")
-  if(!proband %in% carriers)
+  if(!proband %in% c(carriers, homozygous))
     stop2("The proband must be a carrier")
+  if(!is.null(x$LOOP_BREAKERS))
+    stop2("Pedigrees with pre-broken loops are not allowed")
 
-  for(ids in list(proband, affected, unknown, carriers, noncarriers)) {
-    validID = ids %in% labs
-    if(!all(validID))
-      stop2("Unknown ID label: ", ids[!validID])
-  }
+  if(plot)
+    plotSegregation(x, affected, unknown, proband, carriers, homozygous, noncarriers, ...)
 
-  if(length(err1 <- intersect(affected, unknown)))
-    stop2("Individual specified as both affected and unknown: ", err1)
+  # Sex of carriers/noncarriers (needed for Xchrom)
+  caSex = getSex(x, carriers %||% character(0))    # safeguard against NULL
+  ncSex = getSex(x, noncarriers %||% character(0)) # safeguard against NULL
 
-  if(length(err2 <- intersect(carriers, noncarriers)))
-    stop2("Individual specified as both a carrier and a non-carrier: ", err2)
+  # Add marker objects: disease locus, main marker and proband genotype
+  locAttr = list(afreq = c(a = 1 - freq, b = freq),
+                 chrom = if(Xchrom) "X" else NA)
+  x = x |>
+    addMarker(name = "m", locusAttr = locAttr) |>
+    addMarker(name = "mProband", locusAttr = locAttr) |>
+    addMarker(name = "dis", locusAttr = locAttr) |>
+    setGenotype("m", carriers,    geno = ifelse(Xchrom & caSex == 1, "b", "a/b")) |>
+    setGenotype("m", noncarriers, geno = ifelse(Xchrom & ncSex == 1, "a", "a/a")) |>
+    setGenotype("m", homozygous, geno = "b/b")
 
-  if(is.null(freq) || is.na(freq))
-    stop2("An allele frequency must be specified")
-  if(!is.numeric(freq) || length(freq) != 1 || freq <= 0 || freq >= 1)
-    stop2("The allele frequency must be a single number strictly between 0 and 1")
+  x = x |>
+    setGenotype("mProband", proband, geno = genotype(x, "m", proband))
 
-  if(plot) {
-    plotSegregation(x, affected, unknown, proband, carriers, noncarriers, ...)
+  # Break loops if necessary
+  if(hasUnbrokenLoops(x)) {
+    x = breakLoops(x, loopBreakers = loopBreakers, verbose = FALSE)
+
+    # Add duplicates to input vectors, were needed
+    lb = x$ID[x$LOOP_BREAKERS[, 'orig']]
+
+    if(length(lbAff <- intersect(affected, lb)))
+      affected = unique.default(c(affected, paste0("=", lbAff)))
+
+    if(length(lbUn <- intersect(unknown, lb)))
+      unknown = unique.default(c(unknown, paste0("=", lbUn)))
+
+    # TODO: Fix this (easy)
+    if(!is.null(liability))
+      stop2("Liability classes are not yet implemented when `x` has loops")
+
+    # TODO: Handle proband vs loop breaking better
+    if(proband %in% lb)
+      stop2("The proband cannot be a loop breaker; please select different loop breaker(s)")
   }
 
   # Affection status vector, sorted along labs
-  aff = logical(pedsize(x))
-  aff[internalID(x, affected)] = TRUE
-  aff[internalID(x, unknown)] = NA
+  affvec = logical(pedsize(x))
+  affvec[internalID(x, affected)] = TRUE
+  affvec[internalID(x, unknown)] = NA
 
-  # Empty marker (= disease locus)
-  dis = m = mProband = marker(x, afreq = c(a = 1 - freq, b = freq))
+  # Penetrances
+  penetMat = penet2matrix(penetrances, Xchrom = Xchrom)
 
-  # Full marker
-  genotype(m, carriers) = c("a", "b")
-  genotype(m, noncarriers) = c("a", "a")
+  # Liability classes
+  if(is.null(liability))
+    liability = rep_len(1, pedsize(x))
+  else if(length(liability) != pedsize(x))
+    stop2("Pedigree size (", pedsize(x), ") and assigned liability classes (", length(liability), ") must be equal")
 
-  # Proband marker
-  genotype(mProband, proband) = c("a", "b")
+  if(Xchrom) {
+    mls = males(x, internal = TRUE)
 
-  # Penetrances and liability classes
-  penetMat = fixPenetrances(penetrances)
-  liability = liability %||% rep_len(1, pedsize(x))
-  if(!all(liability %in% 1:nrow(penetMat)))
-    stop2("Illegal liability class: ", setdiff(liability, 1:nrow(penetrances)))
+    ilc_male = setdiff(liability[mls], 1:nrow(penetMat$male))
+    ilc_female = setdiff(liability[-mls], 1:nrow(penetMat$female))
+    if(length(ilc_male) | length(ilc_female))
+      stop2("Illegal liability class:",
+            if(length(ilc_male)) c(paste(" male", ilc_male[1]), ilc_male[-1]),
+            if(length(ilc_male) && length(ilc_female)) ";",
+            if(length(ilc_female)) c(paste(" female", ilc_female[1]), ilc_female[-1]))
+  }
+  else
+    if(!all(liability %in% 1:nrow(penetMat)))
+      stop2("Illegal liability class: ", setdiff(liability, 1:nrow(penetMat)))
 
 
   # Setup for likelihood under causal hypothesis
   peelOrder = peelingOrder(x)
-  peeler = pedprobr:::.peel_M_AUT
   peelingProcess = pedprobr:::peelingProcess
 
-  startCausal = function(x, locus)
-    startdata_causative(x, locus, aff = aff, penetMat = penetMat, liability = liability)
+  if(Xchrom) {
+    peeler = function(x, locus) function(dat, sub) pedprobr:::.peel_M_X(dat, sub, SEX = x$SEX)
+    startCausal = function(x, locus) startdata_causative_X(x, locus, aff = affvec, penetMat = penetMat, liability = liability)
+  }
+  else {
+    peeler = function(x, locus) function(dat, sub) pedprobr:::.peel_M_AUT(dat, sub)
+    startCausal = function(x, locus) startdata_causative(x, locus, aff = affvec, penetMat = penetMat, liability = liability)
+  }
 
   likelihoodCausal = function(x, locus) {
     peelingProcess(x, locus, startdata = startCausal,
-                   peeler = peeler, peelOrder = peelOrder)
+                   peeler = peeler(x, locus), peelOrder = peelOrder)
   }
+
+  dis = getMarkers(x, "dis")[[1]]
+  m = getMarkers(x, "m")[[1]]
+  mProband = getMarkers(x, "mProband")[[1]]
+
   # Main Bayes factor
   numer1 = likelihoodCausal(x, m)
   likDis = likelihoodCausal(x, dis)
@@ -136,8 +212,66 @@ FLB = function(x, carriers, noncarriers = NULL, freq,
   FLB = BF1 * BF2
 
   if(details)
-    return(list(c(FLB = FLB, BF1 = BF1, BF2 = BF2),
+    return(list(c(FLB = FLB, BF1 = BF1, `1/BF2` = BF2),
                 c(numer1 = numer1, denom1 = denom1, numer2 = numer2, denom2 = denom2),
                 c(likM = likM, likMproband = likMproband, likDis = likDis)))
   FLB
+}
+
+
+
+
+checkInput = function(x, proband, affected, unknown, carriers, noncarriers,
+                      homozygous, freq = NULL, Xchrom = FALSE,
+                      requireProband = FALSE, requireFreq = FALSE) {
+
+  if(!is.ped(x))
+    stop2("The first argument must be a connected pedigree")
+
+  # Conversion to character unless NULL
+  asChar = function(v) if(!is.null(v)) as.character(v) else NULL
+
+  proband = asChar(proband)
+  affected = asChar(affected)
+  unknown = asChar(unknown)
+  carriers = asChar(carriers)
+  noncarriers = asChar(noncarriers)
+  homozygous = asChar(homozygous)
+
+  # Proband checks
+  if(requireProband && (length(proband) == 0 || proband == ""))
+    stop2("A proband must be specified")
+  if(length(proband) > 1)
+    stop2("At most one proband is permitted")
+  if(length(proband) == 1 && !proband %in% x$ID)
+    stop2("Unknown proband: ", proband)
+
+  # Other input checks
+  allids = c(affected, unknown, carriers, noncarriers, homozygous)
+  if(any(!allids %in% x$ID))
+    stop2("Unknown ID label: ", setdiff(allids, x$ID))
+
+  if(length(err1 <- intersect(affected, unknown)))
+    stop2("Individual specified as both affected and unknown: ", err1)
+
+  if(length(err2 <- intersect(carriers, noncarriers)))
+    stop2("Individual specified as both a carrier and a non-carrier: ", err2)
+
+  if(length(err3 <- intersect(carriers, homozygous)))
+    stop2("Individual specified as both a (heterozygous) carrier and homozygous: ", err3)
+
+  if(length(err4 <- intersect(noncarriers, homozygous)))
+    stop2("Individual specified as both homozygous and a non-carrier: ", err4)
+
+  if(Xchrom && length(err5 <- intersect(males(x), homozygous)))
+    stop2("Male individual specified as a homozygous carrier in an X-linked inheritance model: ", err5)
+
+  # Frequency checks
+  if(requireFreq && is.null(freq))
+      stop2("An allele frequency must be specified")
+  if(!is.null(freq) && !isProb(freq, len = 1))
+      stop2("The allele frequency must be a single number strictly between 0 and 1")
+
+  list(proband = proband, affected = affected, unknown = unknown, carriers = carriers,
+       noncarriers = noncarriers, homozygous = homozygous)
 }
--- R/plotSegregation.R
+++ R/plotSegregation.R
@@ -4,51 +4,147 @@
 #'
 #' @inheritParams FLB
 #' @param cex,margins Arguments passed on to [pedtools::plot.ped()].
+#' @param pos.geno Position of genotype labels relative to pedigree symbols;
+#'   either "bottom" (default), "topleft" or "topright".
+#' @param pos.arrow Position of the proband arrow; either "bottomleft",
+#'   "bottomright", "topleft" or "topright".
 #'
 #' @examples
 #'
 #' x = nuclearPed(2)
-#' plotSegregation(x, affected = 3:4, unknown = 1:2, proband = 3,
-#'                 carriers = 3:4, margins = c(1,3,1,1))
+#' plotSegregation(x, proband = 3, carriers = 3:4, noncarriers = 1,
+#'                 aff = 3:4, unknown = 1:2)
+#'
+#' # Same with various options
+#' plotSegregation(x, proband = 3, carriers = 3:4, noncarriers = 1,
+#'                 aff = 3:4, unknown = 1:2,
+#'                 pos.geno = "topright", pos.arrow = "topleft",
+#'                 labs = NULL, title = "Family 1", cex.main = 1.5)
+#'
+#' # Recessive example
+#' y = cousinPed(1, child = TRUE)
+#' plotSegregation(y, affected = 9, unknown = 1:6, carrier = 7:8,
+#'                 homozygous = 9, noncarriers = c(4,6), proband = 9)
+#'
+#' # Different symbol placements
+#' plotSegregation(y, affected = 9, unknown = 1:6, carrier = 7:8,
+#'                 homozygous = 9, noncarriers = c(4,6), proband = 9,
+#'                 pos.geno = "topleft", pos.arrow = "bottomright")
+#'
+#' # Incest case
+#' y = nuclearPed() |> addChildren(father = 3, mother = 2, nch = 3)
+#'
+#' plotSegregation(y, proband = 4, aff = 4:6, unknown = 2, carrier = 4:6, deceased = 1,
+#'                 pos.geno = "topleft", pos.arrow = "bottomright")
 #'
 #' @importFrom graphics arrows strheight text
 #' @export
 plotSegregation = function(x, affected = NULL, unknown = NULL, proband = NULL,
-                           carriers = NULL, noncarriers = NULL, cex = 1,
-                           margins = rep(1, 4), ...) {
+                           carriers = NULL, homozygous = NULL, noncarriers = NULL, cex = 1,
+                           margins = 1, pos.geno = "bottom", pos.arrow = "bottomleft", ...) {
 
-  # Input checks
-  allids = c(proband, affected, unknown, carriers, noncarriers)
-  if(any(!allids %in% labels(x)))
-    stop2("Unknown ID label: ", setdiff(allids, labels(x)))
+  inputs = checkInput(x, affected = affected, unknown = unknown, proband = proband,
+                      carriers = carriers, homozygous = homozygous, noncarriers = noncarriers)
+  proband = inputs$proband
+  affected = inputs$affected
+  unknown = inputs$unknown
+  carriers = inputs$carriers
+  homozygous = inputs$homozygous
+  noncarriers = inputs$noncarriers
 
-  if(length(err1 <- intersect(affected, unknown)))
-    stop2("Individual specified as both affected and unknown: ", err1)
+  hasProband = length(proband) > 0
 
-  if(length(err2 <- intersect(carriers, noncarriers)))
-    stop2("Individual specified as both a carrier and a non-carrier: ", err2)
+  pos.geno = match.arg(pos.geno, c("bottom", "topleft", "topright"))
+  pos.arrow = match.arg(pos.arrow, c("bottomleft", "bottomright", "topleft", "topright"))
 
-  p = plot(x,
-           aff = affected,
-           textInside = ifelse(1:pedsize(x) %in% unknown, "?", ""),
-           cex = cex,
-           margins = margins,
-           keep.par = TRUE,
-           ...)
+  # Question mark inside those with unknown affection status
+  qm = rep_len("?", length(unknown))
+  names(qm) = unknown
 
-  vdist = p$boxh + 3.25 * abs(strheight("M", cex = cex))  # vertical dist from top of symbol to "+"
+  # Main alignment
+  align = .pedAlignment(x, ...)
+  xpos = align$x
+  ypos = align$y
+  names(xpos) = names(ypos) = x$ID
+
+  # Invoke automatic margin adjustment if not fully specified
+  if(autoMargins <- length(margins) < 4) {
+    margins = rep_len(margins, 4)
+    xr = align$xrange
+    yr = align$yrange
+
+    # Extra margins for genotype labels
+    typed = c(carriers, noncarriers, homozygous)
+
+    homozL = pos.geno == "topleft" && any(xpos[homozygous] == xr[1])
+    carrL  = pos.geno == "topleft" && any(xpos[typed] == xr[1])
+
+    homozR = pos.geno == "topright" && any(xpos[homozygous] == xr[2])
+    carrR  = pos.geno == "topright" && any(xpos[typed] == xr[2])
+
+    carrT = pos.geno %in% c("topleft", "topright") && any(ypos[typed] == yr[1])
+    carrB = pos.geno == "bottom" && any(ypos[typed] == yr[2])
+
+    # Adjustment for arrow?
+    if(hasProband) {
+      arrowL = xpos[proband] == xr[1] && pos.arrow %in% c("bottomleft", "topleft")
+      arrowR = xpos[proband] == xr[2] && pos.arrow %in% c("bottomright", "topright")
+      arrowT = ypos[proband] == yr[1] && pos.arrow %in% c("topleft", "topright")
+      arrowB = ypos[proband] == yr[2] && pos.arrow %in% c("bottomleft", "bottomright")
+    }
+    else
+      arrowL = arrowR = arrowT = arrowB = FALSE
+
+    # Combine adjustments (geno + arrow)
+    extraMargins = c(if(carrB) 0.75 else if(arrowB) 0.5 else 0,
+                     if(arrowL) 2.5  else if(homozL) 1.5 else if(carrL) 0.75 else 0,
+                     if(arrowT) 1.25 else if(carrT) 1 else 0,
+                     if(arrowR) 2.5  else if(homozR) 1.5 else if(carrR) 0.75 else 0)
+
+    margins = margins + extraMargins
+  }
+
+  # Plot pedigree
+  p = plot(x, aff = affected, textInside = qm, cex = cex,
+           margins = margins, keep.par = TRUE, ...)
+
+  boxw = p$scaling$boxw
+  boxh = p$scaling$boxh
+
+  # Genotype label position
+  lpos = switch(pos.geno,
+                bottom = c(3.25, 3, 0),
+                topleft = c(-3, 2, 0.75),
+                topright = c(-3, 4, 0.75))
+
+  vdist = boxh + lpos[1] * abs(strheight("M", cex = cex))  # vertical dist from top of symbol to "+"
 
   if(!is.null(carriers))
-    text(p$x[carriers], p$y[carriers] + vdist, labels = "+", cex = cex*1.5, font = 1, pos = 3, offset = 0)
+    text(xpos[carriers], ypos[carriers] + vdist, labels = "+", cex = cex*1.5, font = 1, pos = lpos[2], offset = lpos[3])
+
+  if(!is.null(homozygous))
+    text(xpos[homozygous], ypos[homozygous] + vdist, labels = "++", cex = cex*1.5, font = 1, pos = lpos[2], offset = lpos[3])
 
   if(!is.null(noncarriers))
-    text(p$x[noncarriers], p$y[noncarriers] + vdist, labels = "-", cex = cex*1.5, font = 1, pos = 3, offset = 0)
+    text(xpos[noncarriers], ypos[noncarriers] + vdist, labels = "-", cex = cex*1.5, font = 1, pos = lpos[2], offset = lpos[3])
 
-  # proband arrow
-  if(!is.null(proband)) {
-    prob = internalID(x, proband)
-    corner.x = p$x[prob] - .5*p$boxw
-    corner.y = p$y[prob] + p$boxh
-    arrows(corner.x - 1.7*p$boxw, corner.y + 0.9*p$boxh, corner.x - .5*p$boxw, corner.y , lwd = 2, length = .15, xpd = NA)
+  # Proband arrow
+  if(hasProband) {
+    mod = switch(pos.arrow,
+           bottomleft = list(x = -1, y = 1),
+           bottomright = list(x = 1, y = 1),
+           topleft = list(x = -1, y = 0),
+           topright = list(x = 1, y = 0))
+
+    corner.x = xpos[proband] + .5*mod$x * boxw
+    corner.y = ypos[proband] +    mod$y * boxh
+    arrows(x0 = corner.x + 1.7*mod$x * boxw,
+           y0 = corner.y + 0.9*mod$y * boxh - 0.9*(1-mod$y) * boxh,
+           x1 = corner.x + 0.5*mod$x * boxw,
+           y1 = corner.y,
+           lwd = 2*cex, length = .15, xpd = NA)
   }
+
+  # Return plot parameters
+  invisible(p)
 }
